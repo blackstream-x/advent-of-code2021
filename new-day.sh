d2e9131ededa8b5c1edb9a17dde3140e0549967d
blackstream-x <rainer@blackstream.de>
2021-12-07 08:40:32 +0100

    Updated new_day.sh to generate all files

--- new-day.sh
+++ new-day.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-day=$(date +%d | bc)
+if [ -z "$1" ] ; then
+    day=$(date +%d | bc)
+else
+    day=$(echo $1 | bc)
+fi
 
 todays_solution=solutions/day${day}.py
 
 if [ -f ${todays_solution} ] ; then
     echo "${todays_solution} already exists!"
 else
-    cp --preserve=mode,ownership solutions/template.py ${todays_solution}
+    sed "s/\$DAY/${day}/g" solutions/template > ${todays_solution}
+    chmod +x ${todays_solution}
 fi
+
+todays_test=tests/test_${day}.py
+
+if [ -f ${todays_test} ] ; then
+    echo "${todays_test} already exists!"
+else
+    sed "s/\$DAY/${day}/g" tests/template > ${todays_test}
+fi
+
+todays_example_input=inputs/${day}.example
+todays_puzzle_input=inputs/${day}.txt
+
+touch ${todays_example_input}
+touch ${todays_puzzle_input}
